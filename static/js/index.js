66b0287355e76039c0c1adf67ea9e3ea937a3395
issei morita <isseimorita1@gmail.com>
2018-01-27 17:30:08 +0900

    make getircode form

--- static/js/index.js
+++ static/js/index.js
@@ -1,45 +1,71 @@
-$(function(){
-	console.log('start jquery')	
-	$('#addIRCode').on('submit',function(e){
-		e.preventDefault()
-		console.log($('#name').val())
-		console.log('clicked ircode')
-		$.ajax({
-			url:'/addcode',
-			type:'POST',
-			data:{
-				'name':$('#name').val(),
-				'code':$('#code').val()
-			}
+$(function ()
+{
+	console.log('start jquery')
+	$('#getIRCode')
+		.submit(function (e)
+		{
+			e.preventDefault()
+			$.ajax('/code-from' + $('#memo_no')
+					.val())
+				.done(function (d)
+				{
+					console.log(d);
+				})
 		})
-			.done(function(d){
-				console.log(d)
-				var IRli=$('<li><button type="button" class="btn btn-primary">'+d.name+'</button></li>')
+	$('#addIRCode')
+		.on('submit', function (e)
+		{
+			e.preventDefault()
+			console.log($('#name')
+				.val())
+			console.log('clicked ircode')
+			$.ajax(
+				{
+					url: '/addcode',
+					type: 'POST',
+					data:
+					{
+						'name': $('#name')
+							.val(),
+						'code': $('#code')
+							.val()
+					}
+				})
+				.done(function (d)
+				{
+					console.log(d)
+					var IRli = $('<li><button type="button" class="btn btn-primary">' + d.name + '</button></li>')
 
-				$('button',IRli).click(
-				function(event){
-						event.preventDefault()
-						$.ajax({
-							url:'/code',
-							type:'POST',
-							data:{
-								'code':d.code
-							}
-						})
-							.done(function(data){
-								console.log(data)
+					$('button', IRli)
+						.click(
+							function (event)
+							{
+								event.preventDefault()
+								$.ajax(
+									{
+										url: '/code',
+										type: 'POST',
+										data:
+										{
+											'code': d.code
+										}
+									})
+									.done(function (data)
+									{
+										console.log(data)
+									})
+
 							})
 
-					})
 
-
-				$('#IRCodeList').append(IRli)
-			})
-			})	
-
-
+					$('#IRCodeList')
+						.append(IRli)
+				})
+		})
 
 
 
 
-	})
+
+
+})
